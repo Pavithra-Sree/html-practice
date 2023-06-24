c736a250576a6ab8624a2ee54e487fde716cdd55
Pavithra-Sree <nirmalsree16@gmail.com>
2023-06-24 10:33:34 +0530

    html-project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App(){
+    return(
+        <>
+        <button> Yes </button>
+        <span> 0 </span>
+        <button> No </button>
+        </>
+    )
+}
+
+export default App;
